6fdd8959348df6284db9f59207ec927e8b6d795d
Henriquesoto92 <henriquesoto92@gmail.com>
2022-06-14 17:46:01 -0300

    :tada: start of the react course via "hora de codar"

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Olá React!</h1>
+      <p>Meu Primeiro App</p>
+    </div>
+  );
+}
+
+export default App;

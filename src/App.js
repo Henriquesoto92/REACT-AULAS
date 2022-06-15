6b18d84bc10dbf0261e546c09e9da84bee5fbc78
Henriquesoto92 <henriquesoto92@gmail.com>
2022-06-15 14:37:33 -0300

    :construction: create Components in REACT

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import './App.css';
 //area de fora superior serve para importar itens
+import './App.css'
+import HelloWorld from './components/HelloWord'
+
+
 function App() {
   //area interna serve para criar os itens da função
 
@@ -17,12 +20,15 @@ function App() {
     //forma correta de declarar "class" no react é usar "className"
     //DICA: sempre observar o console do navegador, ele da alguns avisos silenciosos
     <div className="App">
+
       <h2> Alterando o JSX</h2>
       <p>Olá, me chamo {name}</p>
       <p>Olá, me chamo {newName}</p>
       <p>soma: {2 + 2}</p>
-      <p>soma: {sum(2 + 2)}</p>
+      <p>soma: {sum(2, 2)}</p>
       <img src={url} alt="minha imagem" />
+
+      <HelloWorld />
 
     </div>
   );
---
+++ src/components/HelloWord.js
@@ -0,0 +1,14 @@
+import Frase from './frase'
+
+function HelloWorld() {
+    return /* html */ (
+        <div>
+            <Frase />
+            <h1>Hello World</h1>
+            <Frase />
+            <Frase />
+        </div>
+    )
+}
+
+export default HelloWorld

d1b40078fb130262bc11ac464502163599ecc405
Henriquesoto92 <henriquesoto92@gmail.com>
2022-06-15 14:05:27 -0300

    :memo: class on how to change JSX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
-
+//area de fora superior serve para importar itens
 function App() {
+  //area interna serve para criar os itens da função
+
+  const name = 'Henrique';
+  const newName = name.toUpperCase();
+
+  function sum(a, b) {
+    return a + b
+  };
+
+  const url = "https://via.placeholder.com/150"
+
   return (
+    //<div class="App">
+    //forma correta de declarar "class" no react é usar "className"
+    //DICA: sempre observar o console do navegador, ele da alguns avisos silenciosos
     <div className="App">
-      <h1> Olá React!</h1>
-      <p>Meu Primeiro App</p>
+      <h2> Alterando o JSX</h2>
+      <p>Olá, me chamo {name}</p>
+      <p>Olá, me chamo {newName}</p>
+      <p>soma: {2 + 2}</p>
+      <p>soma: {sum(2 + 2)}</p>
+      <img src={url} alt="minha imagem" />
+
     </div>
   );
 }
-
+//area de fora inferior serve para exportar a função itens
 export default App;

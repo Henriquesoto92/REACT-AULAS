33aa2460dd85ebbae2269a1573e156e0d6c70d38
Henriquesoto92 <henriquesoto92@gmail.com>
2022-06-21 19:00:37 -0300

    :construction: use React Icons

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,10 +1,17 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+
+import styles from './Footer.module.css';
+
 function Footer() {
     return (
-        <>
-            <div>
-                rodapé
-            </div>
-        </>
+        <footer>
+            <ul className={styles.socialList}>
+                <li ><FaFacebook /></li>
+                <li ><FaInstagram /></li>
+                <li ><FaLinkedin /></li>
+            </ul>
+            <p>Nosso Rodapé</p>
+        </footer>
     )
 }
 export default Footer
